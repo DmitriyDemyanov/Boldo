2afec8f9edd8259ad7526d2f512708740f6a98e8
Dmitriy <dimaprostokosmos@gmail.com>
2022-12-23 12:38:41 +0200

    start about

---
+++ src/components/PhotoSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="grid-container">
+    <img id="photo-1" src="@/assets/photo-1.png" alt="photo">
+    <img id="photo-2" src="@/assets/photo-2.png" alt="photo">
+    <img id="photo-3" src="@/assets/photo-3.png" alt="photo">
+    <img id="photo-4" src="@/assets/photo-4.png" alt="photo">
+    <img id="photo-5" src="@/assets/photo-5.png" alt="photo">
+  </div>
+</template>
+
+<script>
+export default {
+name: 'PhotoSection',
+}
+</script>
+
+<style scoped>
+.grid-container {
+  margin-top: -96px;
+
+  display: grid;
+  grid-template-columns: 3fr 4fr 3fr;
+  grid-template-rows: min(285px) min(285px);
+  column-gap: 50px;
+  row-gap: 40px;
+}
+.grid-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+#photo-3 {
+  grid-column-start: 2;
+  grid-row-start: 1;
+  grid-row-end: 3;
+}
+</style>
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,13 +1,64 @@
 <template>
-  <div>hello</div>
+  <div>
+    <div class="bg-header">
+      <div class="container-common">
+        <div class=" wrap-text">
+          <div class="subtitle">About</div>
+          <div class="title">We love to make great things, things that matter.</div>
+          <div class="descr">Funding handshake buyer business-to-business metrics iPad partnership. First mover
+            advantage
+            innovator success deployment non-disclosure.</div>
+        </div>
+      </div>
+    </div>
+    <div class="container-common position-relative">
+      <PhotoSection/>
+    </div>
+  </div>
 </template>
 
 <script>
+import PhotoSection from '@/components/PhotoSection';
 export default {
-name: 'AboutPage',
+  name: 'AboutPage',
+  components: {
+    PhotoSection,
+  }
 }
 </script>
 
 <style scoped>
+.bg-header {
+  background-color: #0A2640;
+  width: 100%;
+  min-height: 615px;
+}
 
+.wrap-text {
+  padding: 155px 154px 0 154px;
+  text-align: center;
+  font-family: 'Open Sans';
+  font-weight: 400;
+  color: #F1F1F1;
+}
+
+.subtitle {
+  font-size: 20px;
+  line-height: 32px;
+}
+
+.title {
+  font-family: 'Manrope';
+  font-weight: 400;
+  font-size: 64px;
+  line-height: 84px;
+  color: #FFFFFF;
+  margin-top: 12px;
+  margin-bottom: 17px;
+}
+
+.descr {
+  font-size: 16px;
+  line-height: 28px;
+}
 </style>
